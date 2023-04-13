5887829120a4ca7c662a568c4724dca7c7ee96c3
K3fas <peter.branecky2@gmail.com>
2023-04-13 17:04:09 +0200

    Cleaned IBasicControlelr Testing pull request CI

--- FVEDoc.Api.App/Controllers/IBasicController.cs
+++ FVEDoc.Api.App/Controllers/IBasicController.cs
@@ -5,23 +5,13 @@ namespace FVEDoc.Api.App.Controllers;
 
 public interface IBasicController<TModel> where TModel : IModelBase
 {
-    [HttpGet]
-    [Route("all")]
     public IEnumerable<TModel> GetAll();
 
-    [HttpGet]
-    [Route("{id?}")]
     public TModel? GetById(Guid id);
 
-    [HttpPost]
-    [Route("create")]
     public IActionResult? Create(TModel model);
 
-    [HttpDelete]
-    [Route("delete/{id?}")]
     public void Delete(Guid id);
 
-    [HttpPut]
-    [Route("update")]
     public Guid? Update(TModel model);
 }
